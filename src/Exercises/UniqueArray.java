95bd270145c0cea5c86c2c8e54b2a3871ef172ab
Bolaji Disu <abolajiidiisu@gmail.com>
2022-09-13 18:27:40 +0100

    Generate Unique Set Of Random Numbers

--- src/Exercises/UniqueArray.java
+++ src/Exercises/UniqueArray.java
@@ -8,18 +8,20 @@ public class UniqueArray {
     public static int[] generateUniqueSet(int numberRange){
         Random rand = new Random();
         int[] storeSet = new int[numberRange];
-
+        int count = 0;
 
         for (int i = 0; i < storeSet.length; i++) {
             int newRand = 1 + rand.nextInt(numberRange);
             storeSet[i] = newRand;
         }
 
-         for (int i = 0; i < storeSet.length; i++) {
+       for (int i = 0; i < storeSet.length; i++) {
             for (int j = i + 1; j < storeSet.length; j++) {
                 if(storeSet[i] == storeSet[j]) {
-                    storeSet[i] = 0;
+                    storeSet[i] += 1+ rand.nextInt(numberRange) + j;
+                    count += 1;
                 }
+
             }
         }
 
@@ -39,20 +41,28 @@ public class UniqueArray {
 //            }
 //
 //        }
+
+
+//        for (int i = 0; i < storeSet.length; i++) {
+//            if(storeSet[i] == 0){
+//                storeSet[i] = 1 + rand.nextInt(numberRange) + 5;
+//            }
+//        }
         return storeSet;
     }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
-        System.out.println(Arrays.toString(generateUniqueSet(5)));
+        System.out.println(Arrays.toString(generateUniqueSet(10)));
+        System.out.println(Arrays.toString(generateUniqueSet(15)));
+        System.out.println(Arrays.toString(generateUniqueSet(20)));
+        System.out.println(Arrays.toString(generateUniqueSet(25)));
+
+//        System.out.println(Arrays.toString(generateUniqueSet(5)));
+//        System.out.println(Arrays.toString(generateUniqueSet(5)));
+//        System.out.println(Arrays.toString(generateUniqueSet(5)));
+//        System.out.println(Arrays.toString(generateUniqueSet(5)));
+//        System.out.println(Arrays.toString(generateUniqueSet(5)));
 //        int newRand;
 //        Random rand   = new Random();
 //        int[] storeSet = new int[10];
