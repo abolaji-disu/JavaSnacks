0833f5e0a10da24cfe3734bff3b70b6b39250cc9
Bolaji Disu <abolajiidiisu@gmail.com>
2022-10-04 13:19:02 +0100

    refactor referee class

--- src/SeptemberLeaveSnack/TicTacToe/TicTacToeReferee.java
+++ src/SeptemberLeaveSnack/TicTacToe/TicTacToeReferee.java
@@ -6,40 +6,40 @@ public class TicTacToeReferee {
     public boolean checkWinInFirstRow() {
         boolean firstRowO = board[0][0] == PlayerMark.O && board[0][1] == PlayerMark.O && board[0][2] == PlayerMark.O;
         boolean firstRowX = board[0][0] == PlayerMark.X && board[0][1] == PlayerMark.X && board[0][2] == PlayerMark.X;
-        if (firstRowX) System.out.print("Player X Wins");
-        if (firstRowO) System.out.print("Player O Wins");
+        if (firstRowX) System.out.println("Player >>X<< Wins !!!");
+        if (firstRowO) System.out.println("Player >>0<< Wins !!!");
         return firstRowX || firstRowO;
     }
 
     public boolean checkWinInSecondRow() {
         boolean firstRowO = board[1][0] == PlayerMark.O && board[1][1] == PlayerMark.O && board[1][2] == PlayerMark.O;
         boolean firstRowX = board[1][0] == PlayerMark.X && board[1][1] == PlayerMark.X && board[1][2] == PlayerMark.X;
-        if (firstRowX) System.out.println("Player X Wins");
-        if (firstRowO) System.out.print("Player O Wins");
+        if (firstRowX) System.out.println("Player >>X<< Wins !!!");
+        if (firstRowO) System.out.println("Player >>0<< Wins !!!");
         return firstRowX || firstRowO;
     }
 
     public boolean checkWinInThirdRow() {
         boolean firstRowO = board[2][0] == PlayerMark.O && board[2][1] == PlayerMark.O && board[2][2] == PlayerMark.O;
         boolean firstRowX = board[2][0] == PlayerMark.X && board[2][1] == PlayerMark.X && board[2][2] == PlayerMark.X;
-        if (firstRowX) System.out.print("Player X Wins");
-        if (firstRowO) System.out.print("Player O Wins");
+        if (firstRowX) System.out.println("Player >>X<< Wins !!!");
+        if (firstRowO) System.out.println("Player >>0<< Wins !!!");
         return firstRowX || firstRowO;
     }
 
     public boolean checkWinInFirstColumn() {
         boolean firstRowO = board[0][0] == PlayerMark.O && board[1][0] == PlayerMark.O && board[2][0] == PlayerMark.O;
         boolean firstRowX = board[0][0] == PlayerMark.X && board[1][0] == PlayerMark.X && board[2][0] == PlayerMark.X;
-        if (firstRowX) System.out.print("Player X Wins");
-        if (firstRowO) System.out.print("Player O Wins");
+        if (firstRowX) System.out.println("Player >>X<< Wins !!!");
+        if (firstRowO) System.out.println("Player >>0<< Wins !!!");
         return firstRowX || firstRowO;
     }
 
     public boolean checkWinInSecondColumn() {
         boolean firstRowO = board[0][1] == PlayerMark.O && board[1][1] == PlayerMark.O && board[2][1] == PlayerMark.O;
         boolean firstRowX = board[0][1] == PlayerMark.X && board[1][1] == PlayerMark.X && board[2][1] == PlayerMark.X;
-        if (firstRowX) System.out.print("Player X Wins");
-        if (firstRowO) System.out.print("Player O Wins");
+        if (firstRowX) System.out.println("Player >>X<< Wins !!!");
+        if (firstRowO) System.out.println("Player >>0<< Wins !!!");
 
         return firstRowX || firstRowO;
     }
@@ -47,24 +47,24 @@ public class TicTacToeReferee {
     public boolean checkWinInThirdColum() {
         boolean firstRowO = board[0][2] == PlayerMark.O && board[1][2] == PlayerMark.O && board[2][2] == PlayerMark.O;
         boolean firstRowX = board[0][2] == PlayerMark.X && board[1][2] == PlayerMark.X && board[2][2] == PlayerMark.X;
-        if (firstRowX) System.out.print("Player X Wins");
-        if (firstRowO) System.out.print("Player O Wins");
+        if (firstRowX) System.out.println("Player >>X<< Wins !!!");
+        if (firstRowO) System.out.println("Player >>0<< Wins !!!");
         return firstRowX || firstRowO;
     }
 
     public boolean checkWinInFirstDiagonal() {
         boolean firstRowO = board[0][0] == PlayerMark.O && board[1][1] == PlayerMark.O && board[2][2] == PlayerMark.O;
         boolean firstRowX = board[0][0] == PlayerMark.X && board[1][1] == PlayerMark.X && board[2][2] == PlayerMark.X;
-        if (firstRowX) System.out.print("Player X Wins");
-        if (firstRowO) System.out.print("Player O Wins");
+        if (firstRowX) System.out.println("Player >>X<< Wins !!!");
+        if (firstRowO) System.out.println("Player >>0<< Wins !!!");
         return firstRowX || firstRowO;
     }
 
     public boolean checkWinInSecondDiagonal() {
         boolean firstRowO = board[0][2] == PlayerMark.O && board[1][1] == PlayerMark.O && board[2][0] == PlayerMark.O;
         boolean firstRowX = board[0][2] == PlayerMark.X && board[1][1] == PlayerMark.X && board[2][0] == PlayerMark.X;
-        if (firstRowX) System.out.print("Player X Wins");
-        if (firstRowO) System.out.print("Player O Wins");
+        if (firstRowX) System.out.println("Player >>X<< Wins !!!");
+        if (firstRowO) System.out.println("Player >>0<< Wins !!!");
         return firstRowX || firstRowO;
     }
 
